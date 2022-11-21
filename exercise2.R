5cb26401b098d909438af420ba29620454da5f64
Felix Thoemmes <felix.thoemmes@gmail.com>
2022-11-20 20:18:33 -0500

    Add files via upload

---
+++ exercise2.R
@@ -0,0 +1,8 @@
+# File name:    exercise2.R
+# Author:       Felix Thoemmes             
+# Date:         11/1/2022
+
+
+
+# Data            --------------------------------------------------------------
+df2 <- read_csv("https://raw.githubusercontent.com/felixthoemmes/gesis_causal_mediation_2022/main/exercise2.csv")
