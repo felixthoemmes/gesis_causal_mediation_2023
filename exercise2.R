92745332848f06299bd4729d6b0abeae208686e1
Felix Thoemmes <felix.thoemmes@gmail.com>
2024-11-15 10:14:22 -0500

    Update exercise2.R
    added factor(X)

--- exercise2.R
+++ exercise2.R
@@ -5,4 +5,5 @@
 
 
 # Data            --------------------------------------------------------------
-df2 <- read_csv("https://raw.githubusercontent.com/felixthoemmes/gesis_causal_mediation_2022/main/exercise2.csv")
+df2 <- read_csv("https://raw.githubusercontent.com/felixthoemmes/gesis_causal_mediation_2023/main/exercise2.csv")
+df2$X <- factor(df2$X)
