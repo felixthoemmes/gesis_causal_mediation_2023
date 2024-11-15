b322b227056537f09e28cc8a46587dd4aa0d1821
Felix Thoemmes <felix.thoemmes@gmail.com>
2024-11-15 09:47:13 -0500

    Update example1.R
    changed to display the direct effect, as opposed to the difference between direct and total

--- example1.R
+++ example1.R
@@ -75,7 +75,9 @@ cat("c path",round(c,2),"\n",
     "c' path for treated group (X=1)",round(cprime1 + h,2),"\n",
     "a x b path (indirect effect for control group X=0)",round(a*b1,2),"\n",
     "a x b path (indirect effect for treated group X=1)",round(a*(b1+h),2),"\n",
-    "c - c' (indirect effect for control group X=0)",round(c-(cprime1 + h*mean(df2$M[df2$X==0])),2),"\n",
-    "c - c' (indirect effect for control group X=1)",round(c-(cprime1 + h*mean(df2$M[df2$X==1])),2))
+    "c' (direct effect for control group X=0)",round((cprime1 + h*mean(df2$M[df2$X==0])),2),"\n",
+    "c' (direct effect for control group X=1)",round((cprime1 + h*mean(df2$M[df2$X==1])),2))
+
+
 
 
